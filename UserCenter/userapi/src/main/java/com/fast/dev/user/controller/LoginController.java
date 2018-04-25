7a76b8a8a98dfec28e1447d2a3788b16e27a9dd9
lianshufeng <251708339@qq.com>
2018-04-25 21:57:36 +0800

    回滚

--- UserCenter/userapi/src/main/java/com/fast/dev/user/controller/LoginController.java
+++ UserCenter/userapi/src/main/java/com/fast/dev/user/controller/LoginController.java
@@ -1,9 +1,5 @@
 package com.fast.dev.user.controller;
 
-import com.fast.dev.component.mongodb.dao.MongoDao;
-import com.fast.dev.user.dao.UserLogDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +13,6 @@ import java.util.HashMap;
 @Controller
 public class LoginController extends SuperController {
 
-    @Autowired
-    private UserLogDao userLogDao;
-
-
     @RequestMapping("login")
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView("login.html");
@@ -30,10 +22,9 @@ public class LoginController extends SuperController {
 
 
     @RequestMapping("login.json")
-    public Object loginJson(@RequestParam(defaultValue = "xiaofeng")  String userName) {
+    public Object loginJson() {
         ModelAndView modelAndView = new ModelAndView("login.html");
         modelAndView.addObject("name", new Date().getTime());
-        this.userLogDao.insert(userName);
         return new HashMap<String, Object>() {{
             put("1", new Date().getTime());
         }};
