d6435afd5d1be7849872bd85c42c6678227aaf93
lianshufeng <251708339@qq.com>
2018-05-10 19:17:47 +0800

    增加网关

---
+++ centers/ApplicationCenter/GetWayServer/src/main/java/com/fast/dev/getway/GetWayApplication.java
@@ -0,0 +1,25 @@
+package com.fast.dev.getway;
+
+import com.fast.dev.core.boot.ApplicationBootSuper;
+import com.fast.dev.core.mvc.MVCConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Import;
+
+
+@EnableZuulProxy
+@EnableEurekaClient
+public class GetWayApplication extends ApplicationBootSuper {
+
+
+    /**
+     * 默认入口方法
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(GetWayApplication.class, args);
+    }
+
+}
