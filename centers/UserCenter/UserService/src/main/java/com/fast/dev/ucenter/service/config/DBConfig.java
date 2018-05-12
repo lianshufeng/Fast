0b3cc87590780b18cfb190eeaaef996589bcf229
lianshufeng <251708339@qq.com>
2018-05-12 16:10:11 +0800

    更新

---
+++ centers/UserCenter/UserService/src/main/java/com/fast/dev/ucenter/service/config/DBConfig.java
@@ -0,0 +1,13 @@
+package com.fast.dev.ucenter.service.config;
+
+import com.fast.dev.component.mongodb.conf.MongodbConfig;
+import com.fast.dev.component.mongodb.configuration.MongodbConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fast.dev.component.mongodb")
+public class DBConfig {
+
+
+}
---
+++ centers/UserCenter/UserService/src/main/java/com/fast/dev/ucenter/service/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.fast.dev.ucenter.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("test")
+    public String test(){
+        return "999999";
+    }
+
+}
