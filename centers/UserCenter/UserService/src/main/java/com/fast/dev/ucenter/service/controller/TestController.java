31fa4ee7b6b48f37597f01b614e6246a62b17568
lianshufeng <251708339@qq.com>
2018-05-14 01:07:28 +0800

    update

--- centers/UserCenter/UserService/src/main/java/com/fast/dev/ucenter/service/controller/TestController.java
+++ centers/UserCenter/UserService/src/main/java/com/fast/dev/ucenter/service/controller/TestController.java
@@ -1,14 +1,21 @@
 package com.fast.dev.ucenter.service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class TestController {
 
+    @Value("${spring.thymeleaf.cache}")
+    private String cache ;
+
+
     @RequestMapping("test")
     public String test(){
-        return "999999";
+        return cache;
     }
 
 }
