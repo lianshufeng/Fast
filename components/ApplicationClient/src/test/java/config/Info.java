57f00cac72546610fd584e97ebfd2f60072e3a3f
lianshufeng <251708339@qq.com>
2018-05-13 11:09:20 +0800

    增加demo

--- components/ApplicationClient/src/test/java/config/Info.java
+++ components/ApplicationClient/src/test/java/config/Info.java
@@ -5,6 +5,7 @@ import feign.interfaces.FeignService;
 import feign.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 import java.util.HashMap;
 
+@RefreshScope
 @RestController
 public class Info extends ActuatorInfoController {
 
 
 
-    @Value("${server.port}")
-    private int port;
+    @Value("${hi}")
+    private String hi;
+
 
 
     @Override
     public Object info() {
-        return "1111";
+        return String.valueOf(hi);
     }
 }
