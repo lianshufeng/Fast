403b932e1ba106498ad6bc4695ee636a7ce35edf
lianshufeng <251708339@qq.com>
2018-08-21 13:42:56 +0800

    update

--- centers/UserCenter/UserServer/src/main/java/com/fast/dev/ucenter/service/UserserverApplication.java
+++ centers/UserCenter/UserServer/src/main/java/com/fast/dev/ucenter/service/UserserverApplication.java
@@ -1,26 +1,20 @@
 package com.fast.dev.ucenter.service;
 
 import com.fast.dev.acenter.annotation.EnableApplicationClient;
-import com.fast.dev.acenter.annotation.EnableApplicationMonitorClient;
 import com.fast.dev.core.boot.ApplicationBootSuper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 
 
 @ComponentScan("com.fast.dev.ucenter")
 
 //应用中心
 @EnableApplicationClient
-//监控中心
-@EnableApplicationMonitorClient
 public class UserserverApplication extends ApplicationBootSuper {
-
 
     public static void main(String[] args) {
         SpringApplication.run(UserserverApplication.class, args);
     }
-
 
 
 }
