55b95320257a387925a45dd624037da1ae7f5683
lianshufeng <251708339@qq.com>
2018-08-31 01:19:10 +0800

    update

--- centers/UserCenter/UserService/src/main/java/com/fast/dev/ucenter/core/service/UserManagerService.java
+++ centers/UserCenter/UserService/src/main/java/com/fast/dev/ucenter/core/service/UserManagerService.java
@@ -13,17 +13,21 @@ public interface UserManagerService {
 
 
     /**
-     * 查询完成的用户令牌
-     *
+     * 通过令牌查询详情
      * @param token
      * @return
      */
-    @RequestMapping(value = "/ucenter/manager/queryUserToken", method = RequestMethod.POST)
-    public UserTokenModel queryByUserToken(@RequestParam("token") String token);
+    @RequestMapping(value = "/manager/queryUserToken", method = RequestMethod.POST)
+    UserTokenModel queryByUserToken(@RequestParam("token") String token);
 
 
-    @RequestMapping(value = "/ucenter/manager/logout", method = RequestMethod.POST)
-    public boolean logout(@RequestParam("token") String token);
+    /**
+     * 注销
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = "/manager/logout", method = RequestMethod.POST)
+    boolean logout(@RequestParam("token") String token);
 
 
 }
