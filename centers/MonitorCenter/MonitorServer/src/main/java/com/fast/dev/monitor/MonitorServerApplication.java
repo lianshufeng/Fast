7c09b67e6a0f5f8f7996e126692a9a39506a9db2
lianshufeng <251708339@qq.com>
2018-12-24 21:12:11 +0800

    优化了本地环境的配置

--- centers/MonitorCenter/MonitorServer/src/main/java/com/fast/dev/monitor/MonitorServerApplication.java
+++ centers/MonitorCenter/MonitorServer/src/main/java/com/fast/dev/monitor/MonitorServerApplication.java
@@ -1,7 +1,5 @@
 package com.fast.dev.monitor;
 
-import com.fast.dev.acenter.annotation.EnableApplicationClient;
-import com.fast.dev.core.boot.ApplicationBootSuper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -10,9 +8,8 @@ import zipkin.server.internal.EnableZipkinServer;
 
 @EnableTurbine
 @EnableHystrixDashboard
-@EnableApplicationClient
 @EnableZipkinServer
-public class MonitorServerApplication extends ApplicationBootSuper {
+public class MonitorServerApplication {
 
     /**
      * 默认入口方法
